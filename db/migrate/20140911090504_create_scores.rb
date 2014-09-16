ab98d98872d676f46849991bd03f4615347232de
tyoshida <tyoshida@mikilab.doshisha.ac.jp>
2014-09-16 14:38:24 +0900

    インデントとデータベースの訂正

--- db/migrate/20140911090504_create_scores.rb
+++ db/migrate/20140911090504_create_scores.rb
@@ -1,7 +1,7 @@
 class CreateScores < ActiveRecord::Migration
   def change
     create_table :scores do |t|
-      t.string :uid
+      t.string :fb_id
       t.integer :competition_id
       t.integer :score
 
