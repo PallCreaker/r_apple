f8421e129a98db490bee9abe65dad820bf14b84d
tyoshida <tyoshida@mikilab.doshisha.ac.jp>
2014-09-16 23:22:21 +0900

    login/indexのview訂正，追加入力画面へのルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'login/new', to: 'login#new', as: 'user_root'
+  get 'login/index'
+  root :to => 'login#index'
   devise_for :users, :controllers => {
     :sessions           =>  "users/sessions",
     :registrations      =>  "users/registrations",
     :passwords          =>  "users/passwords",
     :omniauth_callbacks =>  "users/omniauth_callbacks"
   }
-  get 'login/index'
-  root:to => "login#index"  
 end
