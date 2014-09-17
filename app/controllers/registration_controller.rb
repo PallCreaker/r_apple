160b3eb7ea2252298b30c1877c57cf616ff1f05d
tyoshida <tyoshida@mikilab.doshisha.ac.jp>
2014-09-18 00:47:23 +0900

    大学名，ニックネームの入力フォーム作成

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,15 +1,26 @@
 class RegistrationController < ApplicationController
 before_action :set_user, only: [:index, :edit, :update]
+
   def index
   end
+
   def edit
   end
+
   def update
-    redirect_to :controller => "registration", :action => "index"
+		if @user.update(user_params)
+			redirect_to :controller => "registration", :action => "index"
+		else
+			render 'edit'
+		end
   end
+	private
+	def set_user
+		@user = current_user
+	end
+	def user_params
+		params.require(:user).permit(:university, :user_name)
+	end
 end
 
-private
-def set_user
-  @user = current_user
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  #get 'users/new', to: 'users#new', as: 'user_root'
-  #get 'users/create'
-  #get 'users/index'
-  #root :to => 'users#index'
   get 'registration/edit', to: 'registration#edit', as: 'user_root'
-  get 'registration/update'
+  patch 'registration/update'
   get 'registration/index'
   root :to => 'registration#index'
-  resources(:user)
+  #resource(:user)
   devise_for :users, :controllers => {
     :sessions           =>  "users/sessions",
     :registrations      =>  "users/registrations",
