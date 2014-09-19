46eff7e24a2da9749dc08ef6d5153652c8221292
kento sasamoto <knote0721@gmail.com>
2014-09-19 18:12:43 +0900

    batch

--- lib/tasks/judege.rb
+++ lib/tasks/judege.rb
@@ -1,8 +1,33 @@
 class ResultBatch
   def self.result
     Competition.where(is_fin: false).each do |competition|
-      my_scpre = Score.where("created_at >= ? AND user_id = ?", Date.yesterday, competition.user_id).maximum('score')
-      comp_score = Score.where("created_at >= ? AND user_id = ?", Date.yesterday, competition.competition_id).maximum('score')
+      #今週の月曜日から最大のScoreを取得
+      my_score = Score.where("created_at >= ? AND user_id = ?", this_day - (this_day.wday - 1), competition.user_id).order('score').last
+      comp_score = Score.where("created_at >= ? AND user_id = ?", this_day - (this_day.wday - 1), competition.competition_id).order('score').last
+      result = Result.new
+      result.competition_id = competition.id
+      if my_score.present? && comp_score.present?
+        result.my_score_id = my_score.id
+        result.competition_score_id = comp_score.id
+        result.desicion = (my_score.score >= comp_score.score)
+
+      elsif my_score.blank? && comp_score.blank?
+        result.my_score_id = Score.where(user_id: competition.user_id).order('score').first.id
+        result.competition_score_id = Score.where(user_id: competition.competition_id).order('score').first.id
+        result.desicion = (Score.where(user_id: competition.user_id).order('score').first.score >= Score.where(user_id: competition.competition_id).order('score').first.score)
+
+      elsif my_score.blank? && comp_score.present?
+        result.my_score_id = Score.where(user_id: competition.user_id).order('score').first.id
+        result.competition_score_id = comp_score.id
+        result.desicion = (Score.where(user_id: competition.user_id).order('score').first.score >= comp_score.score)
+
+      elsif my_score.present? && comp_score.blank?
+        result.my_score_id = my_score.id
+        result.competition_score_id = Score.where(user_id: competition.competition_id).order('score').first.id
+        result.desicion = (my_score.score >= Score.where(user_id: competition.competition_id).order('score').first.score)
+          
+      end
+      Result.save
     end
   end
 end
