39af74971519be7ee29f77b2a4610dcf02d4cd31
kento sasamoto <knote0721@gmail.com>
2014-09-19 16:03:40 +0900

    wip batch

--- lib/tasks/judege.rb
+++ lib/tasks/judege.rb
@@ -1,7 +1,9 @@
-class HelloBatch
-  def self.execute
-    Quiz.create
+class ResultBatch
+  def self.result
+    Competition.where(is_fin: false).each do |competition|
+      Score.where("created_at >= ?",Date.yesterday)
+    end
   end
 end
 
-HelloBatch.execute
+ResultBatch.result
