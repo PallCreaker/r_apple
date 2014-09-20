f6602e257a9a2ac30bda127cced928e27007eb67
tyoshida <tyoshida@mikilab.doshisha.ac.jp>
2014-09-20 13:27:27 +0900

    対戦相手決定日追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,6 @@ class HomeController < ApplicationController
     @competition_username = Competition.where(user_id:current_user.id).last.competition_user.user_name
     @myscore = Score.where(user_id:current_user.id).maximum('score')
     @enemyscore = Score.where(user_id:Competition.where(user_id:current_user.id)).maximum('score')
+    @create_result = Competition.where(user_id:current_user.id).last.created_at
   end
 end
