d2b829b4411f3057cb0fb1918cb3d5d7df976ffe
kazumasa Fujita <pallcreaker@gmail.com>
2014-09-25 10:35:19 +0900

    typo

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,5 +1,5 @@
 class Competition < ActiveRecord::Base
   has_many :results
-  has_many :messagess
+  has_many :messages
   belongs_to :competition_user, class_name: User, foreign_key: :competition_id
 end
