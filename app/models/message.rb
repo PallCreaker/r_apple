ee76c1baf1681a4740a7a295d47244d27c25491a
kazumasa Fujita <pallcreaker@gmail.com>
2014-09-26 03:55:46 +0900

    宣戦布告された人のチャットリンクは作成ok next to do 宣戦布告された人chat画面を作る

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,8 @@ class Message < ActiveRecord::Base
   validates :to_id, presence: true, numericality: { only_integer: true }
   validates :competition_id, presence: true, numericality: { only_integer: true }
   validates :content, presence: { message: '必須です'}
+
+  def self.get_my_message(user_id)
+    self.where(to_id: user_id)
+  end
 end
